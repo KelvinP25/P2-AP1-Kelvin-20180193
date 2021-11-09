0ec8d7f63defabf47ec60e79faf627f7647f17f6
KelvinP25 <kelvincastillo9911@gmail.com>
2021-11-08 21:20:08 -0300

    Conculta Proyecto

--- UI/Consulta/cProyecto.xaml.cs
+++ UI/Consulta/cProyecto.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using P2_KELVIN_20180193.BLL;
+using P2_KELVIN_20180193.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,7 +28,18 @@ namespace P2_KELVIN_20180193.UI.Consulta
 
         private void BuscarButton_Click(object sender, RoutedEventArgs e)
         {
-
+            var listado = new List<Proyectos>();
+            if(CriterioTextBox.Text.Trim().Length > 0)
+            {
+                switch (FiltroComboBox.SelectedIndex)
+                {
+                    //case 0:
+                    //    if(DesdeDatePicker.SelectedDate != null && HastaDatePicker.SelectedDate != null)
+                    //    {
+                    //        listado = ProyectosBLL.GetList(e => e.ProyectoId == UtilidadesBLL.ToInt(CriterioTextBox.Text))
+                    //    }
+                }
+            }
         }
     }
 }
